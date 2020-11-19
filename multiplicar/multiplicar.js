4158a52f6dcab50918d1dbedd9b3097cc06677fb
Freddy Flores <freddyflores6666@gmail.com>
2020-11-19 10:51:33 -0600

    archivos iniciales

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,51 @@
+const colors = require('colors');
+const fs = require('fs');
+const { resolve } = require('path');
+
+let listarTabla = (base, limite = 10 ) => {
+
+  console.log('===================='.red);
+  console.log(`=Tabla de ${base}=`.green);
+  console.log('===================='.red);
+
+
+  for( let i = 1; i <= limite; i++) {
+    console.log( `${ base } * ${ i } = ${ base * i }`)
+  }
+
+}
+
+
+let crearArchivo = ( base, limite = 10 ) => {
+
+  return new Promise( (resolve, reject) => {
+
+    if( !Number(base) ) {
+      reject('No es un número')
+      return
+    }
+
+    let data = ''
+
+    for( let i = 1; i <= limite; i++) {
+      data += `${ base } * ${ i } = ${ base * i }\n`
+    }
+
+    fs.writeFile(`tablas/tabla-${base}-al-${ limite }.txt`, data, (err) => {
+      if (err) 
+          reject (err)
+      else
+          resolve(`tabla-${ base }-al-${ limite }.txt`)
+      
+    });
+        
+      })
+
+}
+
+module.exports = {
+  crearArchivo,
+  listarTabla 
+}
+
+
